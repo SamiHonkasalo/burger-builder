72e6cdbabea45df26beeecdf680d75c7436d318f
Sami Honkasalo <N3227@student.jamk.fi>
2019-09-29 15:50:56 +0300

    Optimized some state updates/rendering

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,15 @@ import React from "react";
 import styles from "./Toolbar.module.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 
 const toolbar = props => (
   <header className={styles.Toolbar}>
-    <div>MENU</div>
-    <Logo />
-    <nav>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
+    <div className={styles.Logo}>
+      <Logo />
+    </div>
+    <nav className={styles.DesktopOnly}>
       <NavigationItems />
     </nav>
   </header>
