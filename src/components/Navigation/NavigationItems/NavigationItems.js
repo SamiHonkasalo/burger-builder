48590baa1b1b30ea476dd7ba3f95eba3d3aa021d
Sami Honkasalo <N3227@student.jamk.fi>
2019-10-04 16:14:03 +0300

    Added user authentication with redux

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,6 +6,11 @@ const navigationItems = props => (
   <ul className={styles.NavigationItems}>
     <NavigationItem link="/">Burger Builder</NavigationItem>
     <NavigationItem link="/orders">Orders</NavigationItem>
+    {!props.isAuthenticated ? (
+      <NavigationItem link="/auth">Authenticate</NavigationItem>
+    ) : (
+      <NavigationItem link="/logout">Logout</NavigationItem>
+    )}
   </ul>
 );
 
